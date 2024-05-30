d1e39ad3dadbffed7aa8535a6873ded3f8bbc83c
walter-eidi <walter-eidi@hotmail.com>
2024-05-30 19:52:50 -0300

    Iniciado loja admin mobile

--- app/src/main/java/com/example/lojaadminmobile/MainActivity.kt
+++ app/src/main/java/com/example/lojaadminmobile/MainActivity.kt
@@ -23,6 +23,7 @@ class MainActivity : ComponentActivity() {
                     Greeting(
                         name = "Android",
                         modifier = Modifier.padding(innerPadding)
+
                     )
                 }
             }
