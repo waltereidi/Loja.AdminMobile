fa84454f9ee1f41ae1b11e3d1f22ec17080928cd
walter-eidi <walter-eidi@hotmail.com>
2024-05-30 19:51:55 -0300

    Iniciado loja admin mobile

---
+++ app/src/main/java/com/example/lojaadminmobile/SpashScreen.kt
@@ -0,0 +1,47 @@
+package com.example.lojaadminmobile
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.lojaadminmobile.ui.theme.LojaAdminMobileTheme
+
+class SpashScreen : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            LojaAdminMobileTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting2(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting2(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview2() {
+    LojaAdminMobileTheme {
+        Greeting2("Android")
+    }
+}
