4ec25a543ff102954bd1e990ab278e541b75d06b
walter-eidi <walter-eidi@hotmail.com>
2024-06-01 20:06:26 -0300

    definido background da tela main ,  iniciado posicionamento das views

--- app/src/main/java/com/example/lojaadminmobile/MainActivity.kt
+++ app/src/main/java/com/example/lojaadminmobile/MainActivity.kt
@@ -16,6 +16,7 @@ import com.example.lojaadminmobile.ui.theme.LojaAdminMobileTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main);
         enableEdgeToEdge()
         setContent {
             LojaAdminMobileTheme {
