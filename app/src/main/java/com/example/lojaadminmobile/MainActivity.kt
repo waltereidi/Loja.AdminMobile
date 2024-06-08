9767a589e74556402e2af9ab73f8a7f3628205ef
walter-eidi <walter-eidi@hotmail.com>
2024-06-07 21:06:17 -0300

    tentando conectar uma chamada de api do retrofit na loja backend

--- app/src/main/java/com/example/lojaadminmobile/MainActivity.kt
+++ app/src/main/java/com/example/lojaadminmobile/MainActivity.kt
@@ -5,7 +5,10 @@ import android.widget.EditText
 import androidx.activity.ComponentActivity
 import com.google.gson.annotations.SerializedName
 import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.Body
 import retrofit2.http.POST
 
@@ -23,7 +26,9 @@ data class LoginResponse(
 class MainActivity : ComponentActivity() {
     private val email: EditText get() = findViewById(R.id.emailEdit)
     private val password: EditText get() = findViewById(R.id.passwordEdit)
-    private val retrofit = Retrofit.Builder().baseUrl("https://localhost:7179/").build()
+    private val retrofit = Retrofit.Builder().baseUrl("https://192.168.0.108:7179")
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
     private val apiService = retrofit.create(AuthenticationApi::class.java)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,7 +38,16 @@ class MainActivity : ComponentActivity() {
     fun submitLogin(view: View) {
         try{
             val body = LoginRequest(email.text.toString(), password.text.toString())
-            apiService.submitLogin(body)
+            var call =apiService.submitLogin(body)
+            call?.enqueue(object : Callback<LoginResponse?> {
+                override fun onResponse(call: Call<LoginResponse?>?, response: Response<LoginResponse?>) {
+                    val myItem =response.body()
+                }
+
+                override fun onFailure(call: Call<LoginResponse?>?, t: Throwable?) {
+                    var error = t
+                }
+            })
         }catch (e:Exception)
         {
             val ex = e;
@@ -43,8 +57,10 @@ class MainActivity : ComponentActivity() {
 }
 
 
+
+
 interface AuthenticationApi {
-    @POST("api/Admin/Authentication/Login")
+    @POST("/api/Admin/Authentication/Login")
     fun submitLogin(@Body body:LoginRequest): Call<LoginResponse?>?
 
 }
