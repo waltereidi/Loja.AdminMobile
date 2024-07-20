26e5d79035b4d93c1e5da9dc3b7549b91f3ef40f
walter-eidi <walter-eidi@hotmail.com>
2024-07-19 21:02:48 -0300

    Iniciado implementação de charts para o kotlin , ainda em busca de um bom pacote para imelementar teste com willianchart

--- app/src/main/java/com/example/lojaadminmobile/Home/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/example/lojaadminmobile/Home/ui/dashboard/DashboardFragment.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.lojaadminmobile.databinding.FragmentDashboardBinding
@@ -12,7 +11,6 @@ import com.example.lojaadminmobile.databinding.FragmentDashboardBinding
 class DashboardFragment : Fragment() {
 
     private var _binding: FragmentDashboardBinding? = null
-
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
@@ -27,11 +25,8 @@ class DashboardFragment : Fragment() {
 
         _binding = FragmentDashboardBinding.inflate(inflater, container, false)
         val root: View = binding.root
+        val barChart = binding.barChartHorizontal
 
-        val textView: TextView = binding.textDashboard
-        dashboardViewModelViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
         return root
     }
      override fun onDestroyView() {
